b9a945fd31d74db0349fd4e767590e3ff95a4d34
go2023 <tetyanago2018@gmail.com>
2024-02-21 19:43:02 -0800

    functionality was implemented

--- src/components/use-outside-click/test.jsx
+++ src/components/use-outside-click/test.jsx
@@ -1,12 +1,20 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
+import useOutsideClick from "./index";
 
 const UseOnclickOutsideTest = () => {
-const [showContent, setShowContent] = useState(false);
+    const [showContent, setShowContent] = useState(false);
+    const ref = useRef();
+    useOutsideClick(ref, () => setShowContent(false));
+
     return (
         <div>
             {
                 showContent
-                    ? (<div>
+                    ? (<div ref={ref}
+                            style={{ border: '2px solid black',
+                                padding: '10px',
+                                background: "#008080",
+                                color: "#FFC300" }}>
                         <h1>This is a random content</h1>
                         <p>Click outside to hide content</p>
                     </div>)
